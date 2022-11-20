06e3ba2d0b71bc55b7975205725e775a0e60131c
duresawata <duresakorroso2019@gmail.com>
2022-11-20 15:21:41 +0300

    mail fix

--- base/views.py
+++ base/views.py
@@ -13,14 +13,21 @@ def contact(request):
         message_name = request.POST['message-name']
         message_subject = request.POST['message-subject']
         message_email = request.POST['message-email']
+
         message = request.POST['message']
+        message_body = f"FROM: {message_email}\n\n{message}"
 
         # send an email
         send_mail(
             subject=message_subject,  # subject
-            message=message,  # message
+            message=message_body,  # message
             from_email=message_email,  # from email
             recipient_list=['duresakorroso2019@gmail.com'],  # to email
         )
-        return render(request, 'base/message.html', {'message_name': message_name})
+        context = {
+            'message_name': message_name,
+            'from_email': message_email,
+            'subject': message_subject,
+        }
+        return render(request, 'base/message.html', context)
     return render(request, 'base/home.html', {})
