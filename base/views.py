9d97808403e5e1017113215c4cda374b88d11323
duresawata <duresakorroso2019@gmail.com>
2022-11-20 15:48:05 +0300

    final mail fix

--- base/views.py
+++ base/views.py
@@ -15,13 +15,11 @@ def contact(request):
         message_email = request.POST['message-email']
 
         message = request.POST['message']
-        message_body = f"FROM: {message_email}\n\n{message}"
-        name = f"New message from {message_name}"
+        message_body = f"FROM: {message_email}\nNAME: {message_name}\n\n{message}"
         # send an email
         send_mail(
-            message_subject,  # subject
-            message_body,  # message
-            name,
+            subject=message_subject,  # subject
+            message=message_body,  #
             from_email=message_email,  # from email
             recipient_list=['duresakorroso2019@gmail.com'],  # to email
         )
