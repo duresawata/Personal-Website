2265c37624297f2968247d32051412c259216e3c
duresawata <duresakorroso2019@gmail.com>
2022-11-20 15:35:22 +0300

    mail fix

--- base/views.py
+++ base/views.py
@@ -16,13 +16,14 @@ def contact(request):
 
         message = request.POST['message']
         message_body = f"FROM: {message_email}\n\n{message}"
-
+        name = f"New message from {message_name}"
         # send an email
         send_mail(
-            subject=message_subject,  # subject
-            message=message_body,  # message
-            from_email=message_email,  # from email
-            recipient_list=['duresakorroso2019@gmail.com'],  # to email
+            message_subject,  # subject
+            message_body,  # message
+            name,
+            message_email,  # from_email
+            ['duresakorroso2019@gmail.com'],  # recipient_list
         )
         context = {
             'message_name': message_name,
